e9dd1905bdd640643e65876c2a293b71c0086947
mohitb07 <bmohit980@gmail.com>
2022-02-10 17:23:41 +0530

    request inside try catch, Tours component is not composite component so there isn't any sense in using an opening and closing tag

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import Loading from "./Loading";
 import Tours from "./Tours";
 
-const url = "https://course-api.com/react-tours-project";
+const URL = "https://course-api.com/react-tours-project";
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -14,11 +14,16 @@ function App() {
   };
 
   const getTours = async () => {
-    const response = await fetch(url);
-    const tours = await response.json();
-    setLoading(false);
-    setTours(tours);
-    console.log(tours);
+    try {
+      const response = await fetch(URL);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+      console.log(tours);
+    } catch (e) {
+      setLoading(false);
+      console.error("Internal Server Error", e.message);
+    }
   };
 
   useEffect(() => {
@@ -27,16 +32,16 @@ function App() {
 
   if (loading) {
     return (
-      <main>
+      <>
         <Loading />
-      </main>
+      </>
     );
   }
   if (tours.length === 0) {
     return (
       <main>
         <h1>No more tours to show</h1>
-        <button className="btn" onClick={() => getTours()}>
+        <button className="btn" onClick={getTours}>
           Refresh
         </button>
       </main>
@@ -45,7 +50,7 @@ function App() {
   return (
     <main>
       <h1 style={{ textAlign: "center" }}>Tour List</h1>
-      <Tours tours={tours} removeTour={removeTour}></Tours>
+      <Tours tours={tours} removeTour={removeTour} />
     </main>
   );
 }
